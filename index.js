be00c95a1c2b5860ac9fb6625549ae09305c6123
lewiscornelius <lewiscornelius7@gmail.com>
2024-02-13 21:48:14 -0500

    Applied starter code

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Packages needed for this app
+const fs = require('fs');
+const path = require("path");
+const inquirer = require("inquirer");
+const generatedMarkdown = require(`./utils/generateMarkdown`);
+
+// Questions array
+const questions = [];
+
+//function to write README file
+function writeToFile(fileName, data){}
+
+//Initializing the app
+function init (){}
+
+// calling initializition
+init();
