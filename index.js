3b99680ecec71a9fa2f9afbf802464a5dc914def
lewiscornelius <lewiscornelius7@gmail.com>
2024-02-13 22:37:09 -0500

    fixed GenerateMarkdown

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 // Packages needed for this app
 const fs = require('fs');
-const path = require("path");
-const inquirer = require("inquirer");
-const generatedMarkdown = require(`./utils/generateMarkdown`);
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // Questions array
 const questions = [
